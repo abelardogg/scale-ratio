1bff97032c7db2f2a00b45d380cd6f6906eb6ec7
Abelardo Gandara <Abelardo Gandara>
2016-12-19 22:39:06 -0700

    calculate while press key on height input

--- app/main-app.js
+++ app/main-app.js
@@ -16,5 +16,15 @@ var $widthInputValue, $multiplerInputValue, $heightInputValue;
     $('#height-input').val($widthInputValue*$multiplerInputValue);
   });
 
+  $('#height-input').keyup(function(){
+    $widthInputValue = parseFloat($(this).val());
+    $multiplerInputValue = parseFloat(($('#multipler-input').val())/100);
+    $heightInputValue = parseFloat($('#height-input').val());
+    console.log('width value: '+$widthInputValue+' type: '+ typeof $widthInputValue);
+    console.log('multipler value: '+$multiplerInputValue+' type: '+ typeof $multiplerInputValue);
+    console.log('height value: '+$heightInputValue+' type: '+ typeof $multiplerInputValue);
+    $('#width-input').val($heightInputValue*$multiplerInputValue);
+  });
+
 //end
 });
