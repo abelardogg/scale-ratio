8bad57f562d170656b4e81a582e7dfeae6d1d6b4
Abelardo Gandara <Abelardo Gandara>
2016-12-19 22:36:32 -0700

    calculate while press key on width input

--- app/main-app.js
+++ app/main-app.js
@@ -1,9 +1,20 @@
 jQuery(function (){
 //start
-$(document).ready(function(){
+var $widthInputValue, $multiplerInputValue, $heightInputValue;
 
-});
+  $(document).ready(function(){
 
+
+  });
+  $('#width-input').keyup(function(){
+    $widthInputValue = parseFloat($(this).val());
+    $multiplerInputValue = parseFloat(($('#multipler-input').val())/100);
+    $heightInputValue = parseFloat($('#height-input').val());
+    console.log('width value: '+$widthInputValue+' type: '+ typeof $widthInputValue);
+    console.log('multipler value: '+$multiplerInputValue+' type: '+ typeof $multiplerInputValue);
+    console.log('height value: '+$heightInputValue+' type: '+ typeof $multiplerInputValue);
+    $('#height-input').val($widthInputValue*$multiplerInputValue);
+  });
 
 //end
 });
