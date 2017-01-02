6e90ef657695ecba5c48cbf2a2bbef9e0308b4bf
Abelardo Gandara <Abelardo Gandara>
2017-01-01 22:23:33 -0700

    switching buttons functionality

--- app/main-app.js
+++ app/main-app.js
@@ -1,23 +1,19 @@
 jQuery(function (){
 //start
 var $widthInputValue, $multiplerInputValue, $heightInputValue;
-var $modeButtons = {};
+var $currentMode;
+var $measureButton=$('#measure-mode'), $percentageButton=$('#percentage-mode');
   $(document).ready(function(){
-    $groupModeButtons = $('#groupModeButtons button');
-    console.log();
-    for (var i = 0; i < $groupModeButtons.length; i++) {
-      $modeButtons[i] = {'id' : $groupModeButtons.eq(i).attr('id'),
-                          'classes' : $groupModeButtons.eq(i).attr('class') }
-    }
-    console.log($modeButtons);
+    $currentMode = 'pixel'
   });
+
   $('#width-input').keyup(function(){
     $widthInputValue = parseFloat($(this).val());
     $multiplerInputValue = parseFloat(($('#multipler-input').val())/100);
     $heightInputValue = parseFloat($('#height-input').val());
-    console.log('width value: '+$widthInputValue+' type: '+ typeof $widthInputValue);
-    console.log('multipler value: '+$multiplerInputValue+' type: '+ typeof $multiplerInputValue);
-    console.log('height value: '+$heightInputValue+' type: '+ typeof $multiplerInputValue);
+    // console.log('width value: '+$widthInputValue+' type: '+ typeof $widthInputValue);
+    // console.log('multipler value: '+$multiplerInputValue+' type: '+ typeof $multiplerInputValue);
+    // console.log('height value: '+$heightInputValue+' type: '+ typeof $multiplerInputValue);
     $('#height-input').val($widthInputValue*$multiplerInputValue);
   });
 
@@ -25,11 +21,45 @@ var $modeButtons = {};
     $widthInputValue = parseFloat($(this).val());
     $multiplerInputValue = parseFloat(($('#multipler-input').val())/100);
     $heightInputValue = parseFloat($('#height-input').val());
-    console.log('width value: '+$widthInputValue+' type: '+ typeof $widthInputValue);
-    console.log('multipler value: '+$multiplerInputValue+' type: '+ typeof $multiplerInputValue);
-    console.log('height value: '+$heightInputValue+' type: '+ typeof $multiplerInputValue);
+    // console.log('width value: '+$widthInputValue+' type: '+ typeof $widthInputValue);
+    // console.log('multipler value: '+$multiplerInputValue+' type: '+ typeof $multiplerInputValue);
+    // console.log('height value: '+$heightInputValue+' type: '+ typeof $multiplerInputValue);
     $('#width-input').val($heightInputValue*$multiplerInputValue);
   });
 
+  $('#measure-mode').on('click', function(){
+    activePixels();
+  });
+
+  $('#percentage-mode').on('click', function(){
+    activePercentage();
+  });
+
+  function activePixels(){
+    if($currentMode != 'pixel'){
+      console.log('turning ON pixel: '+$currentMode);
+      $measureButton.addClass('btn-info');
+      $percentageButton.removeClass('btn-info');
+      $currentMode = 'pixel';
+      console.log('currentMode: '+$currentMode);
+
+    } else{
+      alert('pixels are already active');
+
+    }
+  }
+
+  function activePercentage(){
+    if($currentMode != 'percentage'){
+      console.log('turning ON percentage: '+$currentMode);
+      $percentageButton.addClass('btn-info');
+      $measureButton.removeClass('btn-info');
+      $currentMode = 'percentage';
+      console.log('currentMode: '+$currentMode);
+
+    } else{
+      alert('percentage are already active');
+    }
+  }
 //end
 });
