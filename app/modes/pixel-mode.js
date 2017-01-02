d36afd56a277d12652fabdf40cecf85ee0716dc2
Abelardo Gandara <Abelardo Gandara>
2017-01-01 23:57:42 -0700

    canvas added

--- app/modes/pixel-mode.js
+++ app/modes/pixel-mode.js
@@ -1,6 +1,13 @@
 jQuery(function (){
 //start
 
+$(document).ready(function(){
+  $widthInputValue = parseFloat($('#width-input').val());
+  $multiplerInputValue = parseFloat(($('#multipler-input').val())/100);
+  $heightInputValue = parseFloat($('#height-input').val());
+  $('#height-input').val($widthInputValue*$multiplerInputValue);
+});
+
 $('#width-input').keyup(function(){
   $widthInputValue = parseFloat($(this).val());
   $multiplerInputValue = parseFloat(($('#multipler-input').val())/100);
