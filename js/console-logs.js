cbb69e3e64c3d5cfc4653247bb2c4cf41a948c40
Abelardo Gandara <Abelardo Gandara>
2017-01-01 23:19:23 -0700

    added color logs!

---
+++ js/console-logs.js
@@ -0,0 +1,37 @@
+//colored BOLD fonts
+function greenLog(textToLog){
+  console.log('%c ' + textToLog,'  color: green; font-weight: bold');
+}
+function redLog(textToLog){
+  console.log('%c ' + textToLog,'  color: red; font-weight: bold');
+}
+function blueLog(textToLog){
+  console.log('%c ' + textToLog,'  color: blue; font-weight: bold');
+}
+function purpleLog(textToLog){
+  console.log('%c ' + textToLog,'  color: purple; font-weight: bold');
+}
+// console.log('bold fonts');
+// greenLog('green log');
+// redLog('red log');
+// blueLog('blue log');
+// purpleLog('purple log');
+
+//colored THIN fonts
+function thinGreenLog(textToLog){
+  console.log('%c ' + textToLog,'  color: green');
+}
+function thinRedLog(textToLog){
+  console.log('%c ' + textToLog,'  color: red');
+}
+function thinBlueLog(textToLog){
+  console.log('%c ' + textToLog,'  color: blue');
+}
+function thinPurpleLog(textToLog){
+  console.log('%c ' + textToLog,'  color: purple');
+}
+// console.log('thin (normal) fonts');
+// thinGreenLog('green log');
+// thinRedLog('red log');
+// thinBlueLog('blue log');
+// thinPurpleLog('purple log');
